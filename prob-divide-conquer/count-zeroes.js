67cf9681d5e851c8a4b574e3dbdc92e7a3431e95
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-12-26 19:24:19 -0500

    Finished first part, count-zeroes

--- prob-divide-conquer/count-zeroes.js
+++ prob-divide-conquer/count-zeroes.js
@@ -1,8 +1,21 @@
 function countZeroes(arr) {
-	let leftIdx = 0;
-	let rightIdx = arr.length - 1;
-	let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-	let middleVal = arr[middleIdx];
+	let first = findZeroStart(arr);
+	if (first === -1) return 0;
+	return arr.length - first;
+}
+
+function findZeroStart(arr, left = 0, right = arr.length - 1) {
+	if (right >= left) {
+		let middle = left + Math.floor((right - left) / 2);
+		if ((middle === 0 || arr[middle - 1] === 1) && arr[middle] === 0) {
+			return middle;
+		}
+		else if (arr[middle] === 1) {
+			return findZeroStart(arr, middle + 1, right);
+		}
+		return findZeroStart(arr, left, middle - 1);
+	}
+	return -1;
 }
 
 module.exports = countZeroes;
